9350aafb7d5c6c69a352f04c363edea591e1ae1d
AILA Family Tree <aila@family.app>
2025-12-11 18:42:17 +0000

    auto-commit for c9d25815-4a95-40e7-a83c-aaebfa2a33e6

--- backend_test.py
+++ backend_test.py
@@ -11,7 +11,7 @@ from datetime import datetime
 import time
 
 # Configuration
-BASE_URL = "https://livefamily-tree.preview.emergentagent.com/api"
+BASE_URL = "https://familytree-7.preview.emergentagent.com/api"
 TIMEOUT = 30
 
 class GenealogyAPITester:
