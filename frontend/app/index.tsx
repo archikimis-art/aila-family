1f44aea9de5e40d3654fc2b706b13db75c91be83
AILA Family Tree <aila@family.app>
2025-12-11 13:53:55 +0000

    auto-commit for 2ba50988-7574-4f19-b528-f86544527630

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -166,7 +166,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   featuresContainer: {
-    marginBottom: 60,
+    marginBottom: 100,
   },
   featureItem: {
     flexDirection: 'row',
